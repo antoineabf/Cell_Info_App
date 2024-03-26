f163ca341e7f862552e30904e3414000ac3bd104
antoineabf <ana70@mail.aub.edu>
2024-03-26 16:31:07 +0200

    Implemented statistics route

--- backend/app.py
+++ backend/app.py
@@ -28,10 +28,6 @@ class CellData(db.Model):
         self.timestamp = timestamp
 
 
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return "Hello World!"
-
 @app.route('/cellData', methods=['POST'])
 def add_cell_data():
     try:
@@ -51,3 +47,63 @@ def add_cell_data():
 
     except:
         return jsonify({'error': 'Something went wrong'}), 500
+
+
+@app.route('/statistics', methods=['POST'])
+def get_statistics():
+    data = request.json
+    START_DATE = datetime.strptime(data['start_date'], '%d %b %Y %I:%M %p')
+    END_DATE = datetime.strptime(data['end_date'], '%d %b %Y %I:%M %p')
+    cell_id_user = data['cell_id']
+
+    statistics = CellData.query.filter(
+        CellData.timestamp.between(START_DATE, END_DATE)).all()
+
+    alfa_count = sum(1 for stat in statistics if stat.operator == "Alfa")
+    touch_count = sum(1 for stat in statistics if stat.operator == "Touch")
+    total_count_operator = alfa_count + touch_count
+    percentage_alfa = round((alfa_count / total_count_operator * 100), 2) if total_count_operator != 0 else 0
+    percentage_touch = round((touch_count / total_count_operator * 100), 2) if total_count_operator != 0 else 0
+
+    count_4G = sum(1 for stat in statistics if stat.networkType == "4G")
+    count_3G = sum(1 for stat in statistics if stat.networkType == "3G")
+    count_2G = sum(1 for stat in statistics if stat.networkType == "2G")
+    total_count_network = count_4G + count_3G + count_2G
+
+    percentage_4G = round((count_4G / total_count_network * 100), 2) if total_count_network != 0 else 0
+    percentage_3G = round((count_3G / total_count_network * 100), 2) if total_count_network != 0 else 0
+    percentage_2G = round((count_2G / total_count_network * 100), 2) if total_count_network != 0 else 0
+
+    signal_power_4G = [stat.signalPower for stat in statistics if stat.networkType == "4G"]
+    signal_power_3G = [stat.signalPower for stat in statistics if stat.networkType == "3G"]
+    signal_power_2G = [stat.signalPower for stat in statistics if stat.networkType == "2G"]
+
+    signal_power_avg_4G = round((sum(signal_power_4G) / len(signal_power_4G)), 2) if len(signal_power_4G) != 0 else 0
+    signal_power_avg_3G = round((sum(signal_power_3G) / len(signal_power_3G)), 2) if len(signal_power_3G) != 0 else 0
+    signal_power_avg_2G = round((sum(signal_power_2G) / len(signal_power_2G)), 2) if len(signal_power_2G) != 0 else 0
+
+    signal_power_device = [stat.signalPower for stat in statistics if stat.cell_id == cell_id_user]
+    signal_power_avg_device = round((sum(signal_power_device) / len(signal_power_device)), 2) if len(signal_power_device) != 0 else 0
+
+    sinr_snr_4G = [stat.sinr_snr for stat in statistics if stat.networkType == "4G"]
+    sinr_snr_3G = [stat.sinr_snr for stat in statistics if stat.networkType == "3G"]
+    sinr_snr_2G = [stat.sinr_snr for stat in statistics if stat.networkType == "2G"]
+
+    sinr_snr_avg_4G = round((sum(sinr_snr_4G) / len(sinr_snr_4G)), 2) if len(sinr_snr_4G) != 0 else 0
+    sinr_snr_avg_3G = round((sum(sinr_snr_3G) / len(sinr_snr_3G)), 2) if len(sinr_snr_3G) != 0 else 0
+    sinr_snr_avg_2G = round((sum(sinr_snr_2G) / len(sinr_snr_2G)), 2) if len(sinr_snr_2G) != 0 else 0
+
+    return jsonify({
+        "percentage_alfa": percentage_alfa,
+        "percentage_touch": percentage_touch,
+        "percentage_4G": percentage_4G,
+        "percentage_3G": percentage_3G,
+        "percentage_2G": percentage_2G,
+        "signal_power_avg_4G": signal_power_avg_4G,
+        "signal_power_avg_3G": signal_power_avg_3G,
+        "signal_power_avg_2G": signal_power_avg_2G,
+        "signal_power_avg_device": signal_power_avg_device,
+        "sinr_snr_avg_4G": sinr_snr_avg_4G,
+        "sinr_snr_avg_3G": sinr_snr_avg_3G,
+        "sinr_snr_avg_2G": sinr_snr_avg_2G
+    }), 200
